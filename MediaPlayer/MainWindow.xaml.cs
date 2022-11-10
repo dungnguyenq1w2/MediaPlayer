acbf34530e7f0e8e97856385551a87c01527fccb
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2022-11-10 23:04:32 +0700

    Gui playlist and search file

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,11 +22,31 @@ namespace MediaPlayer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        ObservableCollection<MediaFile> _mediaFiles = new ObservableCollection<MediaFile>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string filename = "playlist.txt";
+            string[] lines = File.ReadAllLines(filename);
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                string[] tokens = lines[i].Split(new string[] { " " }, StringSplitOptions.None);
+
+                _mediaFiles.Add(new MediaFile(tokens[0], tokens[1], tokens[2]));
+            }
+
+            DataContext = this;
+            playListView.ItemsSource = _mediaFiles;
         }
 
         private void SliDuration_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
@@ -30,9 +54,41 @@ namespace MediaPlayer
 
         }
 
-        private void BtnSeeking_Click(object sender, RoutedEventArgs e)
+        private void ViewPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            if (mediaGrid.ColumnDefinitions[1].Width == new GridLength(0))
+            {
+                mediaGrid.ColumnDefinitions[1].Width = new GridLength(184, GridUnitType.Star);
+            }
+            else
+            {
+                mediaGrid.ColumnDefinitions[1].Width = new GridLength(0);
+            }
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void BtnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public string Keyword { get; set; }
+        private void keywordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Keyword == "")
+            {
+                playListView.ItemsSource = _mediaFiles;
+            }
+            else
+            {
+                var mediaFiles = new ObservableCollection<MediaFile>(_mediaFiles.Where(
+                                    mediaFile => mediaFile.Name.ToLower().Contains(Keyword.ToLower())).ToList());
+                playListView.ItemsSource = mediaFiles;
+            }
         }
     }
 }
---
+++ MediaPlayer/MediaFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer
+{
+    public class MediaFile : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string totalTime { get; set; }
+        public string currentPlayedTime { get; set; }
+
+        public MediaFile(string name, string tolalTime, string currentPlayedTime)
+        {
+            this.Name = name;
+            this.totalTime = tolalTime;
+            this.currentPlayedTime = currentPlayedTime;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
